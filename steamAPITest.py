0866a299579a119734d0439106b003880d571227
HoboMechanistic <dejagerrob@gmail.com>
2018-07-04 17:14:05 +0100

    updated scripts, kinda combining them and no longer hardcode appid. Also expanded the sql row to 3 datatypes rather than 2. Also parse for the discount code. Albeit in a terrible way for now. Todo: add chunking, add multithreading, tidy up.

--- steamAPITest.py
+++ steamAPITest.py
@@ -6,36 +6,63 @@ import requests
 import time
 import datetime
 
-args    = {'appid': '578080'}
-args2   = {'gameid': '578080'}
-
-req     = requests.get('https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/', params=args)
-req2    = requests.get('https://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v2/', params=args2)
-req3    = requests.get('https://store.steampowered.com/api/appdetails/?appids=730&cc=EE&l=english&v=1')
+# Grab a list of all steam app id's using the web API, not sure if this is returning them all
+# but I got no control over that
 req4    = requests.get('http://api.steampowered.com/ISteamApps/GetAppList/v2')
 
-print(req.status_code)
-#print(req.text)
-print("Probed on: ", datetime.datetime.now().strftime("%d/%m/%y::%H-%M-%S"))
-#print(req2.status_code)
-#print(req2.text)
-#print(req3.status_code)
-#print(req3.text)
+#Note start time so I can see how long this takes
+print("Probed run start on: ", datetime.datetime.now().strftime("%d/%m/%y::%H-%M-%S"))
 
-#print(req4.status_code)
-#file = open("apilist.json","w") 
-#file.write(req4.json()) 
-#file.close() 
-
-#req4.encoding = 'UTF-8'
+# Set up some variables we'll need
+sqlContent = []
 firstVal = req4.json()
 subDict = firstVal['applist']
 secondVal = subDict['apps']
 
+# The docs say that reusing the same HTTP connection will use the underlying TCP connection
+# resulting in faster performance but my code runs slower when I use a session other just spamming GETs
+# Not sure what the best solution is, also dont wanna spam steam with 80K get requests whenever I run this...
+s = requests.Session()
+
+# For each AppID that's been returned we wanna parse the json and grab the discount code
+# This is a list in a list in a list in a list in a list .... you get the point, terrible code that needs to be cleant up
+for val in secondVal:
+    
+    jsonAppID = val['appid']        
+    appreq    = s.get('https://store.steampowered.com/api/appdetails/?appids=' + str(val['appid']) + '&cc=EE&l=english&v=1')
+    appfirstVal = appreq.json()   
+    tmpdisc = '-0%'
+
+    try:
+        appsubDict = appfirstVal[str(val['appid'])]
+        appsecondVal = appsubDict['data']
+        appthirdVal = appsecondVal['package_groups']
+        appfourthVal = appthirdVal[0]
+        appfithVal = appfourthVal['subs']
+        appsixVal = appfithVal[0]
+        appseventhVal = appsixVal['percent_savings_text']
+        tmpdisc = appseventhVal
+        print(appseventhVal)
+        #break <-- Debug stuff, uncomment this to jump out when you hit the first discount
+    except:
+        print("Unknown json found, will set to 0%")
+            
+
+    sqlContent.append([val['appid'], val['name'], tmpdisc])    
+
+#Print end run timestamp and create .sql file for use by my PGDB
+print("Probed run end on: ", datetime.datetime.now().strftime("%d/%m/%y::%H-%M-%S"))
+
 file = open("appidlist.sql", "w")
-file.write("INSERT INTO appIdTable ( appId, productName ) VALUES \n")
-for key in secondVal:
+file.write("INSERT INTO appIdTable ( appId, productName, discount ) VALUES \n")
+for key in sqlContent:
+    # Debug stuff
     #print("Key:" + str(key['appid'])  + "\n")
     #print("Name:" + key['name'] + "\n")
-    file.write("(" + str(key['appid']) + "," + key['name']  + ")" + "," + "\n")
+    
+    # Don't print a comma on the last line of the SQL
+    if set(key) == set(sqlContent[-1]):
+        file.write("(" + str(key[0]) + "," + "'" + key[1] + "'" + "," + "'" + key[2] + "'" + ")" + "\n")
+    else:    
+        file.write("(" + str(key[0]) + "," + "'" + key[1] + "'" + "," + "'" + key[2] + "'" + ")" + "," + "\n")
 file.close()    
--- steamGetDisc.py
+++ steamGetDisc.py
@@ -16,14 +16,31 @@ req    = requests.get('https://store.steampowered.com/api/appdetails/?appids=730
 print(req.status_code)
 #print("Probed on: ", datetime.datetime.now().strftime("%d/%m/%y::%H-%M-%S"))
 
+#730
 firstVal = req.json()
-subDict = firstVal['730']
-secondVal = subDict['data']
-thirdVal = secondVal['package_groups']
-#fourthVal = thirdVal['selection_text']
+try:
+    subDict = firstVal['730']
+    secondVal = subDict['data']
+    thirdVal = secondVal['package_groups']
+    fourthVal = thirdVal[0]
+    fithVal = fourthVal['subs']
+    sixVal = fithVal[0]
+    seventhVal = sixVal['percent_savings_text']
+
+    print(seventhVal)
+except:
+    print("Unknown json config \n")
+
+#try:
+#except ValueError:
+#    print("Unknown json configuration \n")
+
+#for val in fithVal:
+#    print("pass")
+#    print(val['percent_savings_text'])
 
 #Put this in to see nice json things
-print(json.dumps(thirdVal, sort_keys=True, indent=4))
+#print(json.dumps(fithVal, sort_keys=True, indent=4))
 
 #file = open("testlist.log", "w")
 #file.write("INSERT INTO appIdTable ( appId, productName ) VALUES \n")
