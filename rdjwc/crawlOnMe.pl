7179358c1f598846f9ee80318b8a97d9959fe375
HoboMechanistic <dejagerrob@gmail.com>
2018-08-08 13:05:48 +0100

    Adding my webcrawler I built to just grab and parse the data I need from HTML. No luck with this python REST stuff.

---
+++ rdjwc/crawlOnMe.pl
@@ -0,0 +1,49 @@
+use strict;
+use warnings;
+use LWP::Simple qw(get);
+use utf8;
+binmode STDOUT, ':utf8';
+
+sub createSQLFile
+{
+	my $filename = 'appidlist.sql';
+	open(my $fh, '>:encoding(UTF-8)', $filename) or die "Could not open file '$filename' $!";
+		print $fh "INSERT INTO appIdTable ( appId, productName, discount ) VALUES\n";
+	close $fh;
+}
+
+sub scrapeDataToFile
+{
+	my ($pageNum) = @_;
+
+	my $url = 'https://store.steampowered.com/search/?os=win%2Cmac%2Clinux&specials=1&page='.$pageNum;
+	my $html = get $url;
+
+	my @appIDData = $html =~ m/<a href="https:\/\/store\.steampowered\.com\/app\/.*"  data-ds-appid="(.*)" onmouseover=/g;
+	my @productNameData = $html =~ m/<div class="col search_name ellipsis">.*\n.*<span class="title">(.*)<\/span>/g;
+	my @discountData = $html =~ m/<div class="col search_discount responsive_secondrow">.*\n.*<span>(.*)<\/span>/g;
+
+	foreach my $val (0..(@appIDData-1))
+	{
+		my $filename = 'appidlist.sql';
+		open(my $fh, '>>:encoding(UTF-8)', $filename) or die "Could not open file '$filename' $!";
+			print $fh "($appIDData[$val], \'$productNameData[$val]\',\'$discountData[$val]\'), \n";
+		close $fh;
+	}
+}
+
+print "Making SQL file if it doesn't exist\n";
+
+createSQLFile() unless -e "appidlist.sql";
+
+print "Done making SQL file if it didn't exist \n";
+
+my $totalRuns = 10;
+$| = 1;
+for (my $i=1; $i <= $totalRuns; $i++) {
+	print "Starting run ...\n";
+	scrapeDataToFile($i);
+	sleep(10);
+	print "Finishing run ...\n";
+}
+
