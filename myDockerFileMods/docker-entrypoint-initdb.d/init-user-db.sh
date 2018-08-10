a0a0c8a08c8f8dfd5019e71142499c3590a762cd
HoboMechanistic <dejagerrob@gmail.com>
2018-08-10 09:59:09 +0100

    Update and commit this so I can fetch it from my VM.

--- myDockerFileMods/docker-entrypoint-initdb.d/init-user-db.sh
+++ myDockerFileMods/docker-entrypoint-initdb.d/init-user-db.sh
@@ -10,7 +10,8 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 CREATE TABLE appIdTable (
 	appid 	int PRIMARY KEY,
-	productname 	varchar(250)
+	productname 	varchar(250),
+	discount	varchar(250)
 );
 EOSQL
 
